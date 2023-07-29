006c17980374d452040b72d8d7151bbd51b8e527
Mohith kumar <murugan28aug@gmail.com>
2023-07-29 23:40:28 +0530

    requirements

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-#2&qo7y8!8fc^m_323961$3%vic#x7(q35)wai2+d3=*b@-)wp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
